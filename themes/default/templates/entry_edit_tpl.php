2b6f3e7039b441e19a8a41e58cf98a1e8875c2c1
Peter Geer <pageer@skepticats.com>
2013-04-20 23:03:15 -0400

    Fixes issue 42 (resolve issue: resolution=RESOLVED status=Fixed)
    Another test.

--- themes/default/templates/entry_edit_tpl.php
+++ themes/default/templates/entry_edit_tpl.php
@@ -59,106 +59,106 @@
 	<div>
 		<textarea id="body" name="body" accesskey="d" title="<?php p_("Post data");?>" rows="18" cols="40"><?php echo @$DATA; ?></textarea>
 	</div>
-<?php
-if (isset($use_js_editor) && System::instance()->sys_ini->value('entryconfig', 'EditorOnBottom', 0)) {
-	include("js_editor.php");
-}
-?>
-<fieldset id="entry_settings">
-<legend><?php p_("Entry settings");?><a href="#dummy">(-)</a></legend>
-<?php if (!$PUBLISHED): ?>
-<div>
-<?php $title = _("Publish this as an article instead of a regular blog entry");?>
-<label for="publisharticle" title="<?php echo $title;?>"><?php p_("Publish as article");?></label>
-<input type="checkbox" id="publisharticle" name="publisharticle" <?php if (isset($GET_SHORT_PATH)) { echo 'checked="checked"'; }?> />
-</div>
-<?php endif; ?>
-<?php if (!$PUBLISHED): ?>
-<div>
-<?php
-$title = _("Auto-publish this entry at this date");
-$date_string = $AUTO_PUBLISH_DATE ? strftime('%Y-%m-%dT%H:%M:%S', strtotime($AUTO_PUBLISH_DATE)) : '';
-?>
-<label for="autopublishdate" title="<?php echo $title;?>"><?php p_("Auto-publish date");?></label>
-<input type="datetime-local" id="autopublishdate" name="autopublishdate" value="<?php echo $date_string ; ?>" />
-</div>
-<? endif; ?>
-<!--
-<div>
-<?php $title = _("If this is checked, then when you add an item to be uploaded, a link or image tag for it will be added in the body of the entry at the current cursor location.");?>
-<label for="adduploadlink" title="<?php echo $title;?>"><?php p_("Insert link when adding uploads");?></label>
-<input type="checkbox" id="adduploadlink"  title="<?php echo $title;?>" <?php if (! empty($INSERT_LINKS)) echo 'checked="checked"'; ?> />
-</div>
--->
-<?php if ($num_uploads = System::instance()->sys_ini->value("entryconfig", "AllowInitUpload", 1)): ?>
-<div class="upload_field">
-	<?php for ($i = 1; $i <= $num_uploads; $i++): ?>
+	<?php
+	if (isset($use_js_editor) && System::instance()->sys_ini->value('entryconfig', 'EditorOnBottom', 0)) {
+		include("js_editor.php");
+	}
+	?>
+	<fieldset id="entry_settings">
+	<legend><?php p_("Entry settings");?><a href="#dummy">(-)</a></legend>
+	<?php if (!$PUBLISHED): ?>
 	<div>
-		<label for="upload<?php echo $i;?>">Select file</label>
-		<input type="file" name="upload[]" id="upload<?php echo $i;?>" />
+	<?php $title = _("Publish this as an article instead of a regular blog entry");?>
+	<label for="publisharticle" title="<?php echo $title;?>"><?php p_("Publish as article");?></label>
+	<input type="checkbox" id="publisharticle" name="publisharticle" <?php if (isset($GET_SHORT_PATH)) { echo 'checked="checked"'; }?> />
 	</div>
-	<?php endfor; ?>
-</div>
-<?php endif; ?>
-
-<?php if ($ALLOW_ENCLOSURE || ! empty($ENCLOSURE)): /* Add optional enclosure box.*/?>
-<div>
-<?php $title = _('Enter the URL of the MP3 or other media file for this post.  If you are uploading the file to this post, you can enter just the filename.');?>
-<label for="enclosure" title="<?php echo $title;?>"><?php p_("Enclosure/Podcast URL");?></label>
-<input type="text" name="enclosure" id="enclosure" title="<?php echo $title;?>" value="<?php if (isset($ENCLOSURE)) echo $ENCLOSURE;?>" />
-</div>
-<?php endif; ?>
-
-<?php if (isset($STICKY)): /* Checkbox to make articles "sticky". */ ?>
-<div>
-<?php $title = _("Show a link to this in the articles panel of the sidebar");?>
-<label for="sticky" title="<?php echo $title;?>"><?php p_("Show in sidebar"); ?></label>
-<input id="sticky" name="sticky" title="<?php echo $title;?>" type="checkbox" <?php if ($STICKY) { ?> checked="checked" <?php } ?> />
-</div>
-<?php endif; ?>
-
-<div>
-<?php $title = _("Set the markup type for this entry.  Auto-markup is plain text with clickable URLs, LBcode is a dialect of the BBcode markup popular on web forums, and HTML is raw HTML code.");?>
-<label for="input_mode" title="<?php echo $title;?>"><?php p_("Markup type");?></label>
-<select id="input_mode" name="input_mode" title="<?php echo $title;?>">
-<option value="<?php echo MARKUP_NONE;?>"<?php if ($HAS_HTML == MARKUP_NONE){
-echo ' selected="selected"';}?>><?php p_("Auto-markup");?></option>
-<option value="<?php echo MARKUP_BBCODE;?>"<?php if ($HAS_HTML == MARKUP_BBCODE){
-echo ' selected="selected"';}?>><?php p_("LBcode");?></option>
-<option value="<?php echo MARKUP_HTML;?>"<?php if ($HAS_HTML == MARKUP_HTML){
-echo ' selected="selected"';}?>><?php p_("HTML");?></option>
-</select>
-</div>
-
-<div>
-<?php $title = _("Allow readers to post comments on this entry");?>
-<label for="comments" title="<?php echo $title;?>"><?php p_("Allow comments");?></label>
-<input id="comments" name="comments" title="<?php echo $title;?>" type="checkbox" <?php if (! (isset($COMMENTS) && !$COMMENTS) ) { 
-?>checked="checked"<?php } ?> />
-</div>
-
-<div>
-<?php $title = _("Allow this entry to receive TrackBack pings from other blogs");?>
-<label for="trackbacks" title="<?php echo $title;?>"><?php p_("Allow TrackBacks");?></label>
-<input id="trackbacks" name="trackbacks" title="<?php echo $title;?>" type="checkbox" <?php if (! (isset($TRACKBACKS) && !$TRACKBACKS) ) { 
-?>checked="checked"<?php } ?> />
-</div>
-
-<div>
-<?php $title = _("Allow this entry to receive Pingback pings from other blogs");?>
-<label for="pingbacks" title="<?php echo $title;?>"><?php p_("Allow Pingbacks");?></label>
-<input id="pingbacks" name="pingbacks" title="<?php echo $title;?>" type="checkbox" <?php if (! (isset($PINGBACKS) && !$PINGBACKS) ) { 
-?>checked="checked"<?php } ?> />
-</div>
-
-<div>
-<?php $title = spf_("If this box is checked, then after the entry is published, %s will attempt to send Pingback pings to any URLs in the body of the post.", PACKAGE_NAME);?>
-<label for="send_pingbacks" title="<?php echo $title;?>"><?php p_("Send Pingbacks after posting");?></label>
-<input id="send_pingbacks" name="send_pingbacks" title="<?php echo $title;?>" type="checkbox" <?php if ( @$SEND_PINGBACKS === false ) { 
-?>checked="checked"<?php } ?> />
-</div>
-
-</fieldset>
+	<?php endif; ?>
+	<?php if (!$PUBLISHED): ?>
+	<div>
+	<?php
+	$title = _("Auto-publish this entry at this date");
+	$date_string = $AUTO_PUBLISH_DATE ? strftime('%Y-%m-%dT%H:%M:%S', strtotime($AUTO_PUBLISH_DATE)) : '';
+	?>
+	<label for="autopublishdate" title="<?php echo $title;?>"><?php p_("Auto-publish date");?></label>
+	<input type="datetime-local" id="autopublishdate" name="autopublishdate" value="<?php echo $date_string ; ?>" />
+	</div>
+	<? endif; ?>
+	<!--
+	<div>
+	<?php $title = _("If this is checked, then when you add an item to be uploaded, a link or image tag for it will be added in the body of the entry at the current cursor location.");?>
+	<label for="adduploadlink" title="<?php echo $title;?>"><?php p_("Insert link when adding uploads");?></label>
+	<input type="checkbox" id="adduploadlink"  title="<?php echo $title;?>" <?php if (! empty($INSERT_LINKS)) echo 'checked="checked"'; ?> />
+	</div>
+	-->
+	<?php if ($num_uploads = System::instance()->sys_ini->value("entryconfig", "AllowInitUpload", 1)): ?>
+	<div class="upload_field">
+		<?php for ($i = 1; $i <= $num_uploads; $i++): ?>
+		<div>
+			<label for="upload<?php echo $i;?>">Select file</label>
+			<input type="file" name="upload[]" id="upload<?php echo $i;?>" />
+		</div>
+		<?php endfor; ?>
+	</div>
+	<?php endif; ?>
+	
+	<?php if ($ALLOW_ENCLOSURE || ! empty($ENCLOSURE)): /* Add optional enclosure box.*/?>
+	<div>
+	<?php $title = _('Enter the URL of the MP3 or other media file for this post.  If you are uploading the file to this post, you can enter just the filename.');?>
+	<label for="enclosure" title="<?php echo $title;?>"><?php p_("Enclosure/Podcast URL");?></label>
+	<input type="text" name="enclosure" id="enclosure" title="<?php echo $title;?>" value="<?php if (isset($ENCLOSURE)) echo $ENCLOSURE;?>" />
+	</div>
+	<?php endif; ?>
+	
+	<?php if (isset($STICKY)): /* Checkbox to make articles "sticky". */ ?>
+	<div>
+	<?php $title = _("Show a link to this in the articles panel of the sidebar");?>
+	<label for="sticky" title="<?php echo $title;?>"><?php p_("Show in sidebar"); ?></label>
+	<input id="sticky" name="sticky" title="<?php echo $title;?>" type="checkbox" <?php if ($STICKY) { ?> checked="checked" <?php } ?> />
+	</div>
+	<?php endif; ?>
+	
+	<div>
+	<?php $title = _("Set the markup type for this entry.  Auto-markup is plain text with clickable URLs, LBcode is a dialect of the BBcode markup popular on web forums, and HTML is raw HTML code.");?>
+	<label for="input_mode" title="<?php echo $title;?>"><?php p_("Markup type");?></label>
+	<select id="input_mode" name="input_mode" title="<?php echo $title;?>">
+	<option value="<?php echo MARKUP_NONE;?>"<?php if ($HAS_HTML == MARKUP_NONE){
+	echo ' selected="selected"';}?>><?php p_("Auto-markup");?></option>
+	<option value="<?php echo MARKUP_BBCODE;?>"<?php if ($HAS_HTML == MARKUP_BBCODE){
+	echo ' selected="selected"';}?>><?php p_("LBcode");?></option>
+	<option value="<?php echo MARKUP_HTML;?>"<?php if ($HAS_HTML == MARKUP_HTML){
+	echo ' selected="selected"';}?>><?php p_("HTML");?></option>
+	</select>
+	</div>
+	
+	<div>
+	<?php $title = _("Allow readers to post comments on this entry");?>
+	<label for="comments" title="<?php echo $title;?>"><?php p_("Allow comments");?></label>
+	<input id="comments" name="comments" title="<?php echo $title;?>" type="checkbox" <?php if (! (isset($COMMENTS) && !$COMMENTS) ) { 
+	?>checked="checked"<?php } ?> />
+	</div>
+	
+	<div>
+	<?php $title = _("Allow this entry to receive TrackBack pings from other blogs");?>
+	<label for="trackbacks" title="<?php echo $title;?>"><?php p_("Allow TrackBacks");?></label>
+	<input id="trackbacks" name="trackbacks" title="<?php echo $title;?>" type="checkbox" <?php if (! (isset($TRACKBACKS) && !$TRACKBACKS) ) { 
+	?>checked="checked"<?php } ?> />
+	</div>
+	
+	<div>
+	<?php $title = _("Allow this entry to receive Pingback pings from other blogs");?>
+	<label for="pingbacks" title="<?php echo $title;?>"><?php p_("Allow Pingbacks");?></label>
+	<input id="pingbacks" name="pingbacks" title="<?php echo $title;?>" type="checkbox" <?php if (! (isset($PINGBACKS) && !$PINGBACKS) ) { 
+	?>checked="checked"<?php } ?> />
+	</div>
+	
+	<div>
+	<?php $title = spf_("If this box is checked, then after the entry is published, %s will attempt to send Pingback pings to any URLs in the body of the post.", PACKAGE_NAME);?>
+	<label for="send_pingbacks" title="<?php echo $title;?>"><?php p_("Send Pingbacks after posting");?></label>
+	<input id="send_pingbacks" name="send_pingbacks" title="<?php echo $title;?>" type="checkbox" <?php if ( @$SEND_PINGBACKS === false ) { 
+	?>checked="checked"<?php } ?> />
+	</div>
+	
+	</fieldset>
 
 <div class="threebutton">
 <?php if ($PUBLISHED): ?>
