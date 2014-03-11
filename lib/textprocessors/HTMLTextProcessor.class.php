861d0924d0d652177217d4e828fdf579b08340be
Peter Geer <pageer@skepticats.com>
2014-03-10 22:57:55 -0400

    Unbreak HTML URL fixing. Refs issue 54

--- lib/textprocessors/HTMLTextProcessor.class.php
+++ lib/textprocessors/HTMLTextProcessor.class.php
@@ -4,24 +4,15 @@ class HTMLTextProcessor extends TextProcessor {
 	public $filter_name = 'HTML';
 	
 	protected function fixURI($args) {
-		if (count($args) == 3) {
-			$uri = $args[2];
-		} else {
-			$uri = $args[1];
-		}
-		$uri = $this->fixIndividualURI($uri);
-		if (count($args) == 3) {
-			return 'src="'.$uri.'"';
-		} else {
-			return 'href="'.$uri.'"';
-		}
+		$uri = $this->fixIndividualURI($args[2]);
+		return $args[1] . '="' . $uri . '"';
 	}
 	
 	public function toHTML() {
 		if ($this->entry) {
-			$this->formatted = preg_replace_callback("/src=['\"]([^\:]+)['\"]/",
+			$this->formatted = preg_replace_callback("/(src)=['\"]([^\:]+)['\"]/",
 													 array($this, 'fixURI'), $this->formatted);
-			$this->formatted = preg_replace_callback("/href=['\"]([^\:@]+)['\"]/", 
+			$this->formatted = preg_replace_callback("/(href)=['\"]([^\:@]+)['\"]/", 
 													 array($this, 'fixURI'), $this->formatted);
 		}
 	}
