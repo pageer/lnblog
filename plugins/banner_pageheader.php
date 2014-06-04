725ce9ea45cbc759cb8eb313ec5a1f53ebbac46f
Peter Geer <pageer@skepticats.com>
2014-06-03 22:40:17 -0400

    Minor plugin cleanup, add ChangeLog entries.

--- plugins/banner_pageheader.php
+++ plugins/banner_pageheader.php
@@ -8,23 +8,22 @@
 
 class PageHeader extends Plugin {
 
-	function __construct($do_output=0) {
-		global $SYSTEM;
+	public function __construct($do_output=0) {
 
 		$this->plugin_desc = _("Output a banner for the page.");
-		$this->plugin_version = "0.2.2";
+		$this->plugin_version = "0.2.3";
 		$this->show_desc = false;
 		$this->addOption("show_desc", _("Show description"), false,"checkbox");
 
 		$this->addOption('no_event',
 			_('No event handlers - do output when plugin is created'),
-			$SYSTEM->sys_ini->value("plugins","EventDefaultOff", 0), 
+			System::instance()->sys_ini->value("plugins","EventDefaultOff", 0), 
 			'checkbox');
 
 		parent::__construct();
 
 		if ( $this->no_event || 
-		     $SYSTEM->sys_ini->value("plugins","EventForceOff", 0) ) {
+		     System::instance()->sys_ini->value("plugins","EventForceOff", 0) ) {
 			# If either of these is true, then don't set the event handler
 			# and rely on explicit invocation for output.
 		} else {
@@ -34,29 +33,22 @@ class PageHeader extends Plugin {
 		if ($do_output) $this->output();
 	}
 
-	function output($parm=false) {
+	public function output($parm=false) {
 		$blg = NewBlog();
-		if ($blg->isBlog()) { 
-			?>
-<h1><a href="<?php echo $blg->uri('blog'); ?>" title="<?php echo $blg->description; ?>"><?php echo $blg->name; ?></a></h1>
-<?php 
-			if ($this->show_desc) { ?>
-<h3><?php echo $blg->description;?></h3>
-<?php
-			}
-		} else { 
-?>
-<h1><a href="<?php echo PACKAGE_URL; ?>"><?php echo PACKAGE_NAME; ?></a></h1>
-<?php 
-			if ($this->show_desc) { ?>
-<h3><?php echo PACKAGE_DESCRIPTION; ?></h3>
-<?php 
-			}
-		} 
+		if ($blg->isBlog()): ?>
+<h1><a href="<?php echo $blg->uri('blog')?>" title="<?php echo $blg->description?>"><?php echo $blg->name; ?></a></h1><?php
+			if ($this->show_desc): ?>
+<h3><?php echo $blg->description;?></h3><?php
+			endif;
+		else: ?>
+<h1><a href="<?php echo PACKAGE_URL?>"><?php echo PACKAGE_NAME?></a></h1><?php 
+			if ($this->show_desc): ?>
+<h3><?php echo PACKAGE_DESCRIPTION?></h3><?php 
+			endif;
+		endif;
 	}
 }
 
-global $PLUGIN_MANAGER;
-if (! $PLUGIN_MANAGER->plugin_config->value('pageheader', 'creator_output', 0)) {
+if (! PluginManager::instance()->plugin_config->value('pageheader', 'creator_output', 0)) {
 	$plug = new PageHeader();
 }
