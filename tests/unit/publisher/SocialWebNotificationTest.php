cd0fcaf79cb8a26e42b5a7965f54cf9f9c7f4eba
Peter Geer <pgeer@datto.com>
2020-04-13 22:36:44 -0400

    Make individual tests not error

--- tests/unit/publisher/SocialWebNotificationTest.php
+++ tests/unit/publisher/SocialWebNotificationTest.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once __DIR__.'/../../../vendor/phpxmlrpc/phpxmlrpc/lib/xmlrpc.inc';
+require_once __DIR__.'/../../../vendor/phpxmlrpc/phpxmlrpc/lib/xmlrpcs.inc';
+require_once __DIR__.'/../../../vendor/phpxmlrpc/phpxmlrpc/lib/xmlrpc_wrappers.inc';
+
 use Prophecy\Argument;
 
 class SocialWebNotificationTest extends PublisherTestBase {
