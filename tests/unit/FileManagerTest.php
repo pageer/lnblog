b59d80b632dda71af3b4f7ce45253f425ee11313
Peter Geer <pgeer@datto.com>
2021-08-14 23:16:28 -0400

    Fix file manager tests.

--- tests/unit/FileManagerTest.php
+++ tests/unit/FileManagerTest.php
@@ -17,12 +17,12 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals(1, count($files));
         $this->assertEquals('test.jpg', $files[0]->getName());
-        $this->assertEquals('./path/to/entry/test.jpg', $files[0]->getPath());
+        $this->assertEquals('/path/to/entry/test.jpg', $files[0]->getPath());
     }
 
     public function testGetAll_WhenBlogContainsJpegs_ReturnsOnlyAttachedFiles() {
         $blog = $this->prophet->prophesize('Blog');
-        $blog->localpath()->willReturn('./blog');
+        $blog->localpath()->willReturn('/blog');
         $blog->getManagedFiles()->willReturn(
             [
             'index.php',
@@ -33,7 +33,7 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
             'plugins.xml',
             ]
         );
-        $this->fs->scandir('./blog')->willReturn(
+        $this->fs->scandir('/blog')->willReturn(
             [
             '.',
             '..',
@@ -51,30 +51,31 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
             'feeds',
             ]
         );
-        $this->fs->is_file('./blog/index.php')->willReturn(true);
-        $this->fs->is_file('./blog/pathconfig.php')->willReturn(true);
-        $this->fs->is_file('./blog/blogdata.ini')->willReturn(true);
-        $this->fs->is_file('./blog/ip_ban.txt')->willReturn(true);
-        $this->fs->is_file('./blog/re_ban.txt')->willReturn(true);
-        $this->fs->is_file('./blog/plugins.xml')->willReturn(true);
-        $this->fs->is_file('./blog/test.jpg')->willReturn(true);
-        $this->fs->is_file('./blog/cache')->willReturn(false);
-        $this->fs->is_file('./blog/content')->willReturn(false);
-        $this->fs->is_file('./blog/drafts')->willReturn(false);
-        $this->fs->is_file('./blog/entries')->willReturn(false);
-        $this->fs->is_file('./blog/feeds')->willReturn(false);
+        $this->fs->is_file('/blog/index.php')->willReturn(true);
+        $this->fs->is_file('/blog/pathconfig.php')->willReturn(true);
+        $this->fs->is_file('/blog/blogdata.ini')->willReturn(true);
+        $this->fs->is_file('/blog/ip_ban.txt')->willReturn(true);
+        $this->fs->is_file('/blog/re_ban.txt')->willReturn(true);
+        $this->fs->is_file('/blog/plugins.xml')->willReturn(true);
+        $this->fs->is_file('/blog/test.jpg')->willReturn(true);
+        $this->fs->is_file('/blog/cache')->willReturn(false);
+        $this->fs->is_file('/blog/content')->willReturn(false);
+        $this->fs->is_file('/blog/drafts')->willReturn(false);
+        $this->fs->is_file('/blog/entries')->willReturn(false);
+        $this->fs->is_file('/blog/feeds')->willReturn(false);
+        $this->fs->realpath(Argument::any())->willReturnArgument(0);
 
         $manager = $this->createFileManager($blog->reveal());
         $files = $manager->getAll();
 
         $this->assertEquals(1, count($files));
         $this->assertEquals('test.jpg', $files[0]->getName());
-        $this->assertEquals('./blog/test.jpg', $files[0]->getPath());
+        $this->assertEquals('/blog/test.jpg', $files[0]->getPath());
     }
 
     public function testGetAll_WhenDirectoryScanFails_ThrowsRuntimeException() {
         $entry = $this->createBlogEntry();
-        $this->fs->scandir('./path/to/entry/')->willReturn(false);
+        $this->fs->scandir('/path/to/entry/')->willReturn(false);
 
         $this->expectException(RuntimeException::class);
 
@@ -84,17 +85,17 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
 
     public function testRemove_WhenFileExistsForEntry_ShouldDeleteFile() {
         $entry = $this->createBlogEntry(['test.jpg']);
-        $this->fs->delete('./path/to/entry/test.jpg')->willReturn(true);
+        $this->fs->delete('/path/to/entry/test.jpg')->willReturn(true);
 
         $manager = $this->createFileManager($entry->reveal());
         $manager->remove('test.jpg');
 
-        $this->fs->delete('./path/to/entry/test.jpg')->shouldHaveBeenCalled();
+        $this->fs->delete('/path/to/entry/test.jpg')->shouldHaveBeenCalled();
     }
 
     public function testRemove_WhenRemoveFails_ShouldThrowRuntimeException() {
         $entry = $this->createBlogEntry(['test.jpg']);
-        $this->fs->delete('./path/to/entry/test.jpg')->willReturn(false);
+        $this->fs->delete('/path/to/entry/test.jpg')->willReturn(false);
 
         $this->expectException(RuntimeException::class);
 
@@ -104,7 +105,7 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
 
     public function testRemove_WhenFileNotInRepository_ShouldThrowFileNotFound() {
         $entry = $this->createBlogEntry();
-        $this->fs->delete('./path/to/entry/test.jpg')->willReturn(false);
+        $this->fs->delete('/path/to/entry/test.jpg')->willReturn(false);
 
         $this->expectException(FileNotFound::class);
 
@@ -123,37 +124,37 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
 
     public function testAttach_WhenFileValid_ShouldCopyFile() {
         $entry = $this->createBlogEntry();
-        $this->fs->copy('/path/to/doc.pdf', './path/to/entry/')->willReturn(true);
+        $this->fs->copy('/path/to/doc.pdf', '/path/to/entry/')->willReturn(true);
 
         $manager = $this->createFileManager($entry->reveal());
         $manager->attach('/path/to/doc.pdf');
 
-        $this->fs->copy('/path/to/doc.pdf', './path/to/entry/')->shouldHaveBeenCalled();
+        $this->fs->copy('/path/to/doc.pdf', '/path/to/entry/')->shouldHaveBeenCalled();
     }
 
     public function testAttach_WhenFileAlreadyExists_ShouldRecopyFile() {
         $entry = $this->createBlogEntry();
-        $this->fs->copy('/path/to/doc.pdf', './path/to/entry/')->willReturn(true);
+        $this->fs->copy('/path/to/doc.pdf', '/path/to/entry/')->willReturn(true);
 
         $manager = $this->createFileManager($entry->reveal());
         $manager->attach('/path/to/doc.pdf');
 
-        $this->fs->copy('/path/to/doc.pdf', './path/to/entry/')->shouldHaveBeenCalled();
+        $this->fs->copy('/path/to/doc.pdf', '/path/to/entry/')->shouldHaveBeenCalled();
     }
 
     public function testAttach_WhenNewNameProvidedForBlacklistedFile_ShouldCopyFileToNewName() {
         $entry = $this->createBlogEntry();
-        $this->fs->copy('/path/to/entry.xml', './path/to/entry/example.xml')->willReturn(true);
+        $this->fs->copy('/path/to/entry.xml', '/path/to/entry/example.xml')->willReturn(true);
 
         $manager = $this->createFileManager($entry->reveal());
         $manager->attach('/path/to/entry.xml', 'example.xml');
 
-        $this->fs->copy('/path/to/entry.xml', './path/to/entry/example.xml')->shouldHaveBeenCalled();
+        $this->fs->copy('/path/to/entry.xml', '/path/to/entry/example.xml')->shouldHaveBeenCalled();
     }
 
     public function testAttach_WhenCopyFails_ShouldThrowException() {
         $entry = $this->createBlogEntry();
-        $this->fs->copy('/path/to/doc.pdf', './path/to/entry/')->willReturn(false);
+        $this->fs->copy('/path/to/doc.pdf', '/path/to/entry/')->willReturn(false);
 
         $this->expectException(RuntimeException::class);
 
@@ -199,9 +200,9 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
 
     private function createBlogEntry(array $files = []) {
         $entry = $this->prophet->prophesize('BlogEntry');
-        $entry->localpath()->willReturn('./path/to/entry/');
+        $entry->localpath()->willReturn('/path/to/entry/');
         $entry->getManagedFiles()->willReturn(['index.php', 'entry.xml']);
-        $this->setUpEntryFileListing('./path/to/entry/', $files);
+        $this->setUpEntryFileListing('/path/to/entry/', $files);
         return $entry;
     }
 
@@ -223,6 +224,7 @@ class FileManagerTest extends \PHPUnit\Framework\TestCase
         $this->fs->is_file("$path/.")->willReturn(false);
         $this->fs->is_file("$path/..")->willReturn(false);
         $this->fs->is_file("$path/comments")->willReturn(false);
+        $this->fs->realpath(Argument::any())->willReturnArgument(0);
         
         foreach ($extra_files as $file) {
             $this->fs->is_file("$path/$file")->willReturn(true);
